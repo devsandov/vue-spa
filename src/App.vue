0f99ae5d91fc1ab854788df6a33fe0412671d89d
David <davidsandoval0051@gmail.com>
2024-10-19 03:54:56 -0600

    feature: routing vue added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <nav>
+    <a href="#/">Home</a>
+    <a href="#/features">Features</a>
+    <a href="#/pricing">Pricing</a>
+    <a href="#/contact">Contact</a>
+  </nav>
+  <main class="bg-slate-100">
+    <RouterView />
+  </main>
 </template>
+
+<script lang="ts" setup></script>
